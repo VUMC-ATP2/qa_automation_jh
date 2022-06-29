7867847ccdb941a933914bc112f48fa9a35cccb5
julijahmelnicka <julija.hmelnicka@gmail.com>
2022-06-29 10:39:15 +0300

    Browser Test

--- src/test/java/practice/BrowserTest.java
+++ src/test/java/practice/BrowserTest.java
@@ -1,28 +1,35 @@
 package practice;
 
+import lombok.Getter;
 import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.v85.dom.model.PseudoType;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-@Slf4j
-public class BrowserTest {
-    ChromeDriver driver;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Log4j
+@Getter
+public class BrowserTest {
+    private final String LOCAL_FILE = "file://" + this.getClass().getResource("/elements.html").getPath();
+    ChromeDriver driver;
     @BeforeTest
     public void setProperties() {
-  //      System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_PROPERTY,"true");
+      System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+      Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
     }
 
     @BeforeMethod(alwaysRun = true)
     public void openBrowser(){
         log.info("Initialize ChromeWebDriver");
         this.driver = new ChromeDriver();
-        driver.get("https://google.lv");
     }
 
     @Test
@@ -31,9 +38,23 @@ public class BrowserTest {
         driver.manage().window().maximize();
     }
 
+    @Test
+    public void elementTest() {
+        driver.get(LOCAL_FILE);
+        WebElement nameField = driver.findElement(By.id("fNameID"));
+        nameField.sendKeys("John");
+        System.out.println("");
+        WebElement surnameField = driver.findElement(By.id("lNameID"));
+        surnameField.sendKeys("Doe");
+        WebElement checkInfoButton = driver.findElement(By.id("checkDataID"));
+        checkInfoButton.click();
+    }
+
+
     @AfterMethod(alwaysRun = true)
         public void closeBrowser() {
         log.info("Closing ChromeWebDriver");
         driver.close();
+        driver.quit();
     }
 }
